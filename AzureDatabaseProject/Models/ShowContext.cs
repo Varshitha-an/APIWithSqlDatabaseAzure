caf6fcff4e0e671c1572ab27e5f31c5d4f4d1ecb
Varshitha-an <anvarshitha05@gmail.com>
2021-03-29 12:04:20 +0530

    First Commit

---
+++ AzureDatabaseProject/Models/ShowContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDatabaseProject.Models
+{
+    public class ShowContext:DbContext
+    {
+        public ShowContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>().HasData(new Movie
+            {
+                Id = 100,
+                Name = "Toy Story",
+                Description = "Animated",
+                Duration = 110.20f
+            });
+        }
+    }
+}
